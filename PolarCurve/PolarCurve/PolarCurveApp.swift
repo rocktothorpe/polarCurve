7a789b4f9dba8b5f67ee91617bda3ccb0a231d68
Raquel Bonilla <raquelb.2014@gmail.com>
2022-03-08 16:44:03 -0800

    project init

---
+++ PolarCurve/PolarCurve/PolarCurveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarCurveApp.swift
+//  PolarCurve
+//
+//  Created by Raquel Bonilla on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PolarCurveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
